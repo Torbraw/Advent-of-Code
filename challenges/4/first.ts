91fa4c49c64cc4cec686444197bfa0ddf5fa4c76
Maxime <gendron.maxime1@gmail.com>
2022-12-04 11:11:46 -0500

    feat: Day 4 - First challenge

---
+++ challenges/4/first.ts
@@ -0,0 +1,35 @@
+const input = await Deno.readTextFile("input.txt");
+
+const lines = input.split("\r\n");
+
+const isBeetwen = (value: number, min: number, max: number) => {
+  return value >= min && value <= max;
+};
+
+const areBeetwen = (
+  firstValue: number,
+  secondValue: number,
+  min: number,
+  max: number,
+) => {
+  return isBeetwen(firstValue, min, max) && isBeetwen(secondValue, min, max);
+};
+
+let result = 0;
+
+for (const line of lines) {
+  const assignements = line.split(",").map((assignment) => assignment.split("-"));
+  const [firstAssignment, secondAssignment] = assignements;
+  const [firstStart, firstEnd] = firstAssignment.map((value) => parseInt(value));
+  const [secondStart, secondEnd] = secondAssignment.map((value) => parseInt(value));
+
+  // Check if firstStart and firstEnd is between secondStart and secondEnd
+  if (
+    areBeetwen(firstStart, firstEnd, secondStart, secondEnd) ||
+    areBeetwen(secondStart, secondEnd, firstStart, firstEnd)
+  ) {
+    result++;
+  }
+}
+
+console.log(result);
