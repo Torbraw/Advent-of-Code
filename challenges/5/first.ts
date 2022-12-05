87f16f21235b2e8195307d3c91e7bea82b4d557c
Maxime <gendron.maxime1@gmail.com>
2022-12-05 01:25:11 -0500

    feat: Day 5 - First challenge

---
+++ challenges/5/first.ts
@@ -0,0 +1,34 @@
+const input = await Deno.readTextFile("input.txt");
+
+const [cratesRowsString, directions] = input.split("\r\n\r\n");
+
+const cratesRows = cratesRowsString.split("\r\n").reverse();
+const numberOfCratesRow = cratesRows.shift()?.split("   ").length || 0;
+
+const stacks = new Map<number, string[]>();
+
+for (let i = 0; i < numberOfCratesRow; i++) {
+  for (const crateRow of cratesRows) {
+    const currentCrate = crateRow.slice(i * 4, i * 4 + 3).trim().match(/\w/)?.[0] || null;
+    if (currentCrate) {
+      stacks.set(i + 1, [...stacks.get(i + 1) || [], currentCrate]);
+    }
+  }
+}
+
+const lines = directions.split("\r\n");
+for (const line of lines) {
+  const numbers = line.match(/\d+/g);
+  const [quantity, from, to] = numbers?.map((n) => parseInt(n)) as number[];
+
+  const currentStack = stacks.get(from) as string[];
+  const cratesToMove: string[] = [];
+  for (let i = 0; i < quantity; i++) {
+    cratesToMove.push(currentStack.pop() as string);
+  }
+  stacks.set(to, [...stacks.get(to) || [], ...cratesToMove]);
+}
+
+const result = Array.from(stacks.values()).map((stack) => stack.pop()).join("");
+
+console.log(result);
